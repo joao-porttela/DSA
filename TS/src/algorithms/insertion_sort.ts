4bd36b604f781f8502aa79dccbbc3b4a98ab7cd2
joao-porttela <joao.portela1901@gmail.com>
2025-02-13 10:52:17 -0300

    insertion sort

---
+++ TS/src/algorithms/insertion_sort.ts
@@ -0,0 +1,13 @@
+export function insertionSort(arr: any[], n: number) {
+  for (let i = 1; i < n; i++) {
+    let curr = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > curr) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = curr;
+  }
+}
